92eaab0b9bc017133e6a8d9484f23e88dbb72642
reddyyuvakishore1211 <79675559+reddyyuvakishore1211@users.noreply.github.com>
2021-09-16 13:29:25 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,20 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+# Create your views here.
+from django.http import HttpResponse
+from app.forms import *
+def imageData(request):
+    form=ImageForm()
+    if request.method=='POST' and request.FILES:
+        form_data=ImageForm(request.POST,request.FILES)
+        if form_data.is_valid():
+            img=form_data.cleaned_data['image']
+            fs=FileSystemStorage()
+            file=fs.save(img.name,img)
+            image_url=fs.url(file)
+            return render(request,'image_display.html',context={'image_url':image_url})
+            
+
+
+    return render(request,'image.html',context={'form':form})
